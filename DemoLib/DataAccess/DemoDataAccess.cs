edfb629f447b6292d16a848d8db341f4927077e4
Gaba54 <72696284+Gaba54@users.noreply.github.com>
2022-03-03 12:53:47 +0100

    DataAccess

---
+++ DemoLib/DataAccess/DemoDataAccess.cs
@@ -0,0 +1,30 @@
+﻿using DemoLib.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DemoLib.DataAccess
+{
+    public class DemoDataAccess : IDataAccess
+    {
+        private List<PersonalModel> people = new(); //c# 9.0
+
+        public DemoDataAccess()
+        {
+            people.Add(new PersonalModel { Id = 1, FirstName = "Tim", LastName = "Corey" });
+            people.Add(new PersonalModel { Id = 2, FirstName = "Suzie", LastName = "Storm" });
+        }
+
+        public List<PersonalModel> GetPeople()
+        {
+            return people;
+        }
+
+        public PersonalModel InsertPerson(string firstname, string lastname)
+        {
+            PersonalModel p = new() { FirstName = firstname, LastName = lastname };
+            p.Id = people.Max(x => x.Id) + 1; //to get a unique Id in database
+            people.Add(p);
+            return p;
+        }
+    }
+}
---
+++ DemoLib/DataAccess/IDataAccess.cs
@@ -0,0 +1,11 @@
+﻿using DemoLib.Models;
+using System.Collections.Generic;
+
+namespace DemoLib.DataAccess
+{
+    public interface IDataAccess
+    {
+        List<PersonalModel> GetPeople();
+        PersonalModel InsertPerson(string firstname, string lastname);
+    }
+}
